603417c2503191fa22924d3283c12fa2d836f1d7
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-04-20 20:58:13 +0200

    Initial commit

---
+++ E08_elektronik/elektronik.py
@@ -0,0 +1,28 @@
+'''
+Created on 20.04.2018
+
+@author: Nils Schmitt
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+import scipy.integrate as integrate
+from scipy.stats import chi2
+from matplotlib import rc
+
+rc('font',**{'family':'serif','serif':['Linux Libertine O']})
+plt.rcParams['errorbar.capsize']=2
+
+
+####################
+#Aufgabe 1#
+####################
+
+f = np.array([1,2,3,4,5,6,7,8,9])
+plt.plot(f,f**2, linestyle='None', marker='.',
+            color='black', label='Messdaten')
+#plt.axis([4e2, 1.5e5, 10, 1.5E3])
+plt.xlabel('g(f)', fontsize=13)
+plt.ylabel('Frequenz [Hz]', fontsize=13)
+plt.title('Abb. [6]: Frequenzgang', fontsize=16)
+plt.show()
