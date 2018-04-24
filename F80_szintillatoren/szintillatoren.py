bcd9211509d8d7ac695ae8a035c00acbde2ff150
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-04-24 15:35:09 +0200

    update data 24.04

--- F80_szintillatoren/szintillatoren.py
+++ F80_szintillatoren/szintillatoren.py
@@ -5,15 +5,43 @@ Created on 20.04.2018
 '''
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import curve_fit
-import scipy.integrate as integrate
-from scipy.stats import chi2
+#from scipy.optimize import curve_fit
+#import scipy.integrate as integrate
+#from scipy.stats import chi2
 from matplotlib import rc
 
 rc('font',**{'family':'serif','serif':['Linux Libertine O']})
 plt.rcParams['errorbar.capsize']=2
 
+####################
+#Aufgabe 4.2#
+####################
+raw = np.loadtxt('data//verstaerkung_4_t62_raw.dat',delimiter = '\t', unpack=True)
+a = raw[:1025]
+plt.plot(a, linestyle='-', marker='.',
+            color='black', label='Messdaten')
+plt.xlabel('Channel', fontsize=13)
+plt.ylabel('Counts', fontsize=13)
+plt.title('Abb. [3]: Coarse Gain 4', fontsize=16)
+plt.show()
 
+raw = np.loadtxt('data//verstaerkung_64_t60_raw.dat',delimiter = '\t', unpack=True)
+a = raw[0:1025]
+plt.plot(a, linestyle='None', marker='.',
+            color='black', label='Messdaten')
+plt.xlabel('Channel', fontsize=13)
+plt.ylabel('Counts', fontsize=13)
+plt.title('Abb. [4]: Coarse Gain 64', fontsize=16)
+plt.show()
+
+raw = np.loadtxt('data//verstaerkung_128_t62_raw.dat',delimiter = '\t', unpack=True)
+a = raw[0:1025]
+plt.plot(a, linestyle='None', marker='.',
+            color='black', label='Messdaten')
+plt.xlabel('Channel', fontsize=13)
+plt.ylabel('Counts', fontsize=13)
+plt.title('Abb. [5]: Coarse Gain 128', fontsize=16)
+plt.show()
 ####################
 #Aufgabe 1#
 ####################
@@ -25,4 +53,4 @@ plt.plot(f,f**2, linestyle='None', marker='.',
 plt.xlabel('g(f)', fontsize=13)
 plt.ylabel('Frequenz [Hz]', fontsize=13)
 plt.title('Abb. [6]: Frequenzgang', fontsize=16)
-plt.show()
+#plt.show()
