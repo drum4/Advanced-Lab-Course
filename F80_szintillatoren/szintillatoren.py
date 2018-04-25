a100287fd95e8fb0a197264d34237859f6ff3492
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-04-25 18:34:30 +0200

    update data

--- F80_szintillatoren/szintillatoren.py
+++ F80_szintillatoren/szintillatoren.py
@@ -66,5 +66,25 @@ plt.show()
 
 
 ####################
-#Aufgabe 1#
+#Aufgabe 5#
 ####################
+
+raw = np.loadtxt('data//verstaerkung_128_t62_raw.dat', 
+                delimiter = '\t', unpack=True)
+a = raw[0:1025]
+plt.plot(a, linestyle='None', marker='.',
+            color='black', label='Messdaten')
+plt.xlabel('Channel', fontsize=13)
+plt.ylabel('Counts', fontsize=13)
+plt.title('Abb. [5]: Coarse Gain 128', fontsize=16)
+plt.text(850,300,'%s'%('Photopeak'),
+         fontsize=13)
+plt.arrow(930, 290, 40, -70, shape='full', width=3, 
+          length_includes_head=True, color='black')
+plt.text(650,120,'%s'%('Compton-Effekt'),
+         fontsize=13)
+plt.arrow(790, 110, 50, -40, shape='full', width=3, 
+          length_includes_head=True, color='black')
+plt.legend(frameon=True, fontsize = 12)
+plt.savefig('figures//f80_abb_5.pdf',format='pdf')
+plt.show()
