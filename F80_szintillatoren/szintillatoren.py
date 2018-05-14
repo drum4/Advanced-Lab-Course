a2b3da397dd74514600ac1950fe0cd93431a5628
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-05-14 11:26:58 +0200

    update energycalib

--- F80_szintillatoren/szintillatoren.py
+++ F80_szintillatoren/szintillatoren.py
@@ -20,14 +20,19 @@ plt.rcParams['errorbar.capsize']=2
 ch = np.array([250,495])
 ch_err = np.array([5,10])
 E = np.array([0.66166,1.3325])
+E_err = np.array([0.00003,0.0003])
 M = np.arange(1025)
 
-def linear (x,a,b):
-    return a*x+b
+def linear (x,a):
+    return a*x
 
 popt, pcov = curve_fit(linear, ch, E,
-                        absolute_sigma=True,
-                        sigma=ch_err)
+                        absolute_sigma=True)
+chisquare = np.sum(((linear(ch,*popt)-E)**2/E_err**2))
+dof = 1
+chisquare_red = chisquare/dof
+steigung = 'm ='+str(np.round(popt[0],4))+' $\\pm$ '+str(np.round(np.sqrt(pcov[0][0]),4))+' MeV'
+chisquare_text = '$\\chi_{red}^2$ = '+str(np.round(chisquare_red,1))
 plt.errorbar(ch, E, xerr=ch_err, fmt=".", linewidth=1,
               linestyle='', color='black',
                label='Messpunkte mit Fehler')
@@ -37,10 +42,10 @@ plt.title('Abb. [16]: Energie als Funktion der Kanäle',
            fontsize=16)
 plt.plot(M, linear(M,*popt),
          color='red', label='Linearer Fit')
-#plt.text(15000,5,'%s \n%s'%(steigung,chisquare_text),
- #        bbox={'facecolor':'white', 'alpha':0.5, 'pad':10},
-  #        fontsize=13)
+plt.text(500,0.5,'%s \n%s'%(steigung,chisquare_text),
+        bbox={'facecolor':'white', 'alpha':0.5, 'pad':10},
+        fontsize=13)
 plt.legend(frameon=True, fontsize = 12)
 plt.savefig('figures//f80_abb_16.pdf',format='pdf')
-#plt.show()
+plt.show()
 plt.close()
