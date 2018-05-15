03f008feac25bb2f0acb162c6fe6f31de2bc9c8e
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-05-15 14:28:28 +0200

    update 14:28

--- F80_szintillatoren/szintillatoren.py
+++ F80_szintillatoren/szintillatoren.py
@@ -26,13 +26,17 @@ def linear (x,a,b):
     return a*x+b
 
 popt, pcov = curve_fit(linear, V, P, absolute_sigma=True)
-plt.errorbar(V, P, xerr=dV, yerr=dP, fmt='.', linewidth=1, linestyle='', label='Messpunkte mit Fehler')
+plt.errorbar(V, P, xerr=dV, yerr=dP,
+             fmt='.', linewidth=1,
+             linestyle='', color='black',
+             label='Messpunkte mit Fehler')
 plt.xlabel('Spannung [V]', fontsize=13)
-plt.ylabel('Pulse height [Channel]', fontsize=13)
-plt.title('Abb. [1]: Position des Photopeaks', fontsize=16)
+plt.ylabel('Pulshöhe [Channel]', fontsize=13)
+plt.title('Abb. [16]: Position des Photopeaks', fontsize=16)
 plt.plot(M, linear(M,*popt), color='red', label='Linearer Fit')
-plt.savefig('figures//f80_abb_1.pdf',format='pdf')
-plt.show()
+plt.savefig('figures//f80_abb_16.pdf',format='pdf')
+#plt.show()
+plt.close()
 
 
 ################
@@ -45,31 +49,35 @@ E = np.array([0.66166,1.3325])
 E_err = np.array([0.00003,0.0003])
 M = np.arange(1025)
 
-def linear (x,a):
+def prop (x,a):
     return a*x
 
-popt, pcov = curve_fit(linear, ch, E,
+popt, pcov = curve_fit(prop, ch, E,
                         absolute_sigma=True)
-chisquare = np.sum(((linear(ch,*popt)-E)**2/E_err**2))
+chisquare = np.sum(((prop(ch,*popt)-E)**2/E_err**2))
 dof = 1
 chisquare_red = chisquare/dof
+prob=np.round(1-chi2.cdf(chisquare,dof),2)*100
+print("Wahrscheinlichkeit Plot=", prob,"%")
+
 steigung = 'm ='+str(np.round(popt[0],4))+' $\\pm$ '+str(np.round(np.sqrt(pcov[0][0]),4))+' MeV'
 chisquare_text = '$\\chi_{red}^2$ = '+str(np.round(chisquare_red,1))
+
 plt.errorbar(ch, E, xerr=ch_err, fmt=".", linewidth=1,
               linestyle='', color='black',
                label='Messpunkte mit Fehler')
-plt.xlabel('Channel', fontsize=13)
+plt.xlabel('Pulshöhe [Channel]', fontsize=13)
 plt.ylabel('Energie [MeV]', fontsize=13)
-plt.title('Abb. [16]: Energie als Funktion der Kanäle',
+plt.title('Abb. [17]: Energie als Funktion der Kanäle',
            fontsize=16)
-plt.plot(M, linear(M,*popt),
+plt.plot(M, prop(M,*popt),
          color='red', label='Linearer Fit')
 plt.text(500,0.5,'%s \n%s'%(steigung,chisquare_text),
         bbox={'facecolor':'white', 'alpha':0.5, 'pad':10},
         fontsize=13)
 plt.legend(frameon=True, fontsize = 12)
-plt.savefig('figures//f80_abb_16.pdf',format='pdf')
-plt.show()
+plt.savefig('figures//f80_abb_17.pdf',format='pdf')
+#plt.show()
 plt.close()
 
 ##############
@@ -80,11 +88,28 @@ x1,y1 = np.loadtxt('data//Sr90_diff.dat',
                  usecols=(1,2), unpack=True)
 x = x1[:1025]
 y = y1[:1025]
+
+#Kurie-Plot
+m_e=511
+alpha=1/137.
+Z=38
+
+
+def fermi (x):
+    return (2*np.pi*(Z*(x+m_e)/(1/alpha*np.sqrt((x**2)+2*m_e*x)))/(1-np.exp(-2*np.pi*(38*(x+m_e)/(1/alpha*np.sqrt((x**2)+2*m_e*x))))))
+
+#fit [500:1600] g(x) "sr90korr.tka" using ($0*k+m):(sqrt($1/(F($0)*($0+m_e)*sqrt($0**2+2*$0*m_e)))) via a,b
+
+#plot "sr90korr.tka" using ($0*k+m):(sqrt($1/(F($0)*($0+511)*sqrt($0**2+2*$0*511)))) title "Kurie-Plot" with points 4, g(x) title "Fit"
+
+
+
 plt.plot(x,y, linestyle='-', marker='.',
             color='black', label='Messdaten')
+plt.plot(x,)
 plt.xlabel('Channel', fontsize=13)
 plt.ylabel('Counts', fontsize=13)
-plt.title('Abb. [17]: Sr90 Spektrum', fontsize=16)
-plt.savefig('figures//f80_abb_17.pdf',format='pdf')
+plt.title('Abb. [18]: Kurie-Plot zur Endpunktsbestimmung der Elektronenenergie von Sr 90', fontsize=16)
+plt.savefig('figures//f80_abb_18.pdf',format='pdf')
 plt.show()
 plt.close()
