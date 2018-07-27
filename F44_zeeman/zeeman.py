0d4b997b470cfde635b37188bdbdbc6056f50b3c
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-07-27 11:29:38 +0200

    update zeeman and laser

---
+++ F44_zeeman/zeeman.py
@@ -0,0 +1,16 @@
+'''
+Created on 27.07.2018
+
+@author: Nils Schmitt
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import rc
+
+rc('font',**{'family':'serif','serif':['Linux Libertine O']})
+plt.rcParams['errorbar.capsize']=2
+
+
+####################
+#Emitterschaltung#
+####################
