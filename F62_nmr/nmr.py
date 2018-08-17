d717ddc4617eba52271f1a23f2de6d82445925b9
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-08-17 02:15:57 +0200

    update first try report

---
+++ F62_nmr/nmr.py
@@ -0,0 +1,12 @@
+'''
+Created on Aug 16, 2018
+
+@author: schmi
+'''
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from matplotlib import rc
+
+rc('font',**{'family':'serif','serif':['Linux Libertine O']})
+plt.rcParams['errorbar.capsize']=2
