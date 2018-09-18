793048fa0d28d19d9165e34e54b583a11f1f7867
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-09-18 17:26:32 +0200

    update 18.9

--- F62_nmr/nmr.py
+++ F62_nmr/nmr.py
@@ -149,31 +149,59 @@ plt.close()
 ##########################
 #Chemical Shift#
 ##########################
-x, y=np.loadtxt('data/samples/Bplus.txt', skiprows=0, unpack=True)
+x, y=np.loadtxt('data/samples/A+.txt', skiprows=0, unpack=True)
 
-plt.plot(x[51:], y[51:], color='black', label='Measuring Data')
+plt.plot(x[110:295], y[110:295], color='black', label='Measuring data')
 plt.xlabel('Frequency [Hz]', fontsize=13)
 plt.ylabel('Amplitude', fontsize=13)
-#plt.title('Fig. [4]: Sample B+', fontsize=16)
-plt.annotate("TMS-Peak", xy=(350, 0.105), xytext=(100, 0.115), fontsize=13,
+#plt.title('Fig. [4]: Sample A+', fontsize=16)
+plt.annotate("TMS-Peak", xy=(325, 1.4), xytext=(250, 3.1), fontsize=13,
             arrowprops=dict(arrowstyle="simple", color='black'))
 plt.legend(frameon=True, fontsize = 12)
 plt.savefig('figures//f61_abb_4.pdf',format='pdf')
 #plt.show()
 plt.close()
 
-x, y=np.loadtxt('data/samples/Cplus.txt', skiprows=0, unpack=True)
+x, y=np.loadtxt('data/samples/Bplus.txt', skiprows=0, unpack=True)
 
-plt.plot(x[51:], y[51:], color='black', label='Measuring Data')
+plt.plot(400,0.14, color='black')
+plt.plot(x[77:350], y[77:350], color='black', label='Measuring data')
 plt.xlabel('Frequency [Hz]', fontsize=13)
 plt.ylabel('Amplitude', fontsize=13)
-#plt.title('Fig. [5]: Sample C+', fontsize=16)
-plt.annotate("TMS-Peak", xy=(650, 0.1), xytext=(740, 0.14), fontsize=13,
+#plt.title('Fig. [5]: Sample B+', fontsize=16)
+plt.annotate("TMS-Peak", xy=(357, 0.1085), xytext=(250, 0.13), fontsize=13,
             arrowprops=dict(arrowstyle="simple", color='black'))
 plt.legend(frameon=True, fontsize = 12)
 plt.savefig('figures//f61_abb_5.pdf',format='pdf')
 #plt.show()
 plt.close()
+
+x, y=np.loadtxt('data/samples/Cplus.txt', skiprows=0, unpack=True)
+
+plt.plot(x[120:450], y[120:450], color='black', label='Measuring data')
+plt.xlabel('Frequency [Hz]', fontsize=13)
+plt.ylabel('Amplitude', fontsize=13)
+#plt.title('Fig. [6]: Sample C+', fontsize=16)
+plt.annotate("TMS-Peak", xy=(650, 0.1), xytext=(720, 0.14), fontsize=13,
+            arrowprops=dict(arrowstyle="simple", color='black'))
+plt.legend(frameon=True, fontsize = 12)
+plt.savefig('figures//f61_abb_6.pdf',format='pdf')
+#plt.show()
+plt.close()
+
+x, y=np.loadtxt('data/samples/D+.txt', skiprows=0, unpack=True)
+
+plt.plot(x[247:450], y[247:450], color='black', label='Measuring data')
+plt.xlabel('Frequency [Hz]', fontsize=13)
+plt.ylabel('Amplitude', fontsize=13)
+#plt.title('Fig. [7]: Sample D+', fontsize=16)
+plt.annotate("TMS-Peak", xy=(603, 2.7), xytext=(540, 4.5), fontsize=13,
+            arrowprops=dict(arrowstyle="simple", color='black'))
+plt.legend(frameon=True, fontsize = 12)
+plt.savefig('figures//f61_abb_7.pdf',format='pdf')
+#plt.show()
+plt.close()
+
 
 
 
@@ -190,14 +218,14 @@ x1, y1=np.loadtxt('data/testProfil1.txt', skiprows=0, unpack=True)
 x2, y2=np.loadtxt('data/testProfil2.txt', skiprows=0, unpack=True)
 x3, y3=np.loadtxt('data/testProfil3.txt', skiprows=0, unpack=True)
 
-plt.plot(x1, y1, color='black', label='Less Oil')
-plt.plot(x2, y2, color='forestgreen', label='Much Oil')
-plt.plot(x3, y3, color='darkblue', label='Oil and Teflon')
-plt.xlabel('Vertical Coordinates [mm]', fontsize=13)
+plt.plot(x1, y1, color='black', label='Less oil')
+plt.plot(x2, y2, color='forestgreen', label='Much oil')
+plt.plot(x3, y3, color='darkblue', label='Oil and teflon')
+plt.xlabel('Vertical coordinates [mm]', fontsize=13)
 plt.ylabel('Amplitude', fontsize=13)
-#plt.title('Fig. [6]: Different Oil', fontsize=16)
+#plt.title('Fig. [8]: Different oil', fontsize=16)
 plt.legend(frameon=True, fontsize = 12)
-plt.savefig('figures//f61_abb_6.pdf',format='pdf')
+plt.savefig('figures//f61_abb_8.pdf',format='pdf')
 #plt.show()
 plt.close()
 
@@ -225,7 +253,7 @@ x10, y10=np.loadtxt('data/oilinsand/testProfil538976376.txt', skiprows=0, unpack
 x11, y11=np.loadtxt('data/oilinsand/testProfil538976386.txt', skiprows=0, unpack=True)
 x12, y12=np.loadtxt('data/oilinsand/testProfil538976396.txt', skiprows=0, unpack=True)
 
-plt.plot(x1[299:], y1[299:], color='black', label='Measured Data')
+plt.plot(x1[299:], y1[299:], color='black', label='Measured data')
 plt.plot(x2[299:], y2[299:])
 #plt.plot(x3[299:], y3[299:])
 #plt.plot(x4[299:], y4[299:])
@@ -237,10 +265,10 @@ plt.plot(x9[299:], y9[299:])
 plt.plot(x10[299:], y10[299:])
 #plt.plot(x11[299:], y11[299:])
 #plt.plot(x12[299:], y12[299:])
-plt.xlabel('Vertical Coordinates [mm]', fontsize=13)
+plt.xlabel('Vertical coordinates [mm]', fontsize=13)
 plt.ylabel('Amplitude', fontsize=13)
-#plt.title('Fig. [7]: Oil in Sand', fontsize=16)
+#plt.title('Fig. [9]: Oil in sand', fontsize=16)
 plt.legend(frameon=True, fontsize = 12)
-plt.savefig('figures//f61_abb_7.pdf',format='pdf')
+plt.savefig('figures//f61_abb_9.pdf',format='pdf')
 #plt.show()
 plt.close()
