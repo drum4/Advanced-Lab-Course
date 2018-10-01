052cce05c659b37d7175e82c2fd1176b1ee74f23
TimoKleinbek <kleinbek@stud.uni-heidelberg.de>
2018-10-01 13:14:25 +0200

    Anfang dritter Teil

--- F16_laserspektroskopie/laser.py
+++ F16_laserspektroskopie/laser.py
@@ -131,6 +131,7 @@ slope50_err=np.sqrt(pcov50[1][1])
 print('slope50=',slope50,'+/-',slope50_err)
 
 
+
 ####################
 # Transmissionpeak #
 ####################
@@ -291,7 +292,6 @@ popt3, pcov3 = curve_fit(Gauss, x_2[1400:1416], trans_2[1400:1416],p0=[0.3,1.41,
 popt4, pcov4 = curve_fit(Gauss, x_2[1453:1486], trans_2[1453:1486],p0=[0.2,1.47,0.006,0], absolute_sigma=True)
 popt5, pcov5 = curve_fit(Gauss, x_2[1536:1556], trans_2[1536:1556],p0=[0.05,1.547,0.006,0], absolute_sigma=True)
 
-
 plt.plot(x_2[1142:1610],trans_2[1142:1610], linestyle='-',color='black', label='Messkurve')
 plt.plot(x_2[1236:1265], Gauss(x_2[1236:1265],*popt1), color='red', label='Gaussian')
 plt.plot(x_2[1330:1355], Gauss(x_2[1330:1355],*popt2), color='red')
@@ -348,11 +348,191 @@ print('f=',f,'+',f_err,'In der Abbildung der StromFrequenzCharakteristik sieht m
 ######### Frequenz Temperatur Charakteristik #########
 
 x_2, trans_2 = np.loadtxt('data/temperatur.csv', delimiter=',', usecols=(3, 4), unpack=True)
-plt.plot(x_2,trans_2, linestyle='-',color='black', label='Messkurve')
+popt1, pcov1 = curve_fit(Gauss, x_2[350:450], trans_2[350:450],p0=[0.3,1.6,0.04,0], absolute_sigma=True)
+popt2, pcov2 = curve_fit(Gauss, x_2[576:704], trans_2[576:704],p0=[0.3,2.5,0.04,0], absolute_sigma=True)
+popt3, pcov3 = curve_fit(Gauss, x_2[781:881], trans_2[781:881],p0=[0.3,3.3,0.04,0], absolute_sigma=True)
+popt4, pcov4 = curve_fit(Gauss, x_2[968:1059], trans_2[968:1059],p0=[0.3,4.0,0.04,0], absolute_sigma=True)
+popt5, pcov5 = curve_fit(Gauss, x_2[1134:1225], trans_2[1134:1225],p0=[0.3,4.7,0.04,0], absolute_sigma=True)
+popt6, pcov6 = curve_fit(Gauss, x_2[1364:1473], trans_2[1364:1473],p0=[0.3,5.6,0.04,0], absolute_sigma=True)
+popt7, pcov7 = curve_fit(Gauss, x_2[1531:1621], trans_2[1531:1621],p0=[0.3,6.3,0.04,0], absolute_sigma=True)
+popt8, pcov8 = curve_fit(Gauss, x_2[1698:1789], trans_2[1698:1789],p0=[0.3,6.9,0.04,0], absolute_sigma=True)
+popt9, pcov9 = curve_fit(Gauss, x_2[1841:1956], trans_2[1841:1956],p0=[0.3,7.6,0.04,0], absolute_sigma=True)
+
+plt.plot(x_2[85:2240],trans_2[85:2240], linestyle='-',color='black', label='Messkurve')
+plt.plot(x_2[350:450], Gauss(x_2[350:450],*popt1), color='red', label='Gaussian')
+plt.plot(x_2[576:704], Gauss(x_2[576:704],*popt2), color='red')
+plt.plot(x_2[781:881], Gauss(x_2[781:881],*popt3), color='red')
+plt.plot(x_2[968:1059], Gauss(x_2[968:1059],*popt4), color='red')
+plt.plot(x_2[1134:1225], Gauss(x_2[1134:1225],*popt5), color='red')
+plt.plot(x_2[1364:1473], Gauss(x_2[1364:1473],*popt6), color='red')
+plt.plot(x_2[1531:1621], Gauss(x_2[1531:1621],*popt7), color='red')
+plt.plot(x_2[1698:1789], Gauss(x_2[1698:1789],*popt8), color='red')
+plt.plot(x_2[1841:1956], Gauss(x_2[1841:1956],*popt9), color='red')
 plt.xlabel('Zeit t [s]', fontsize=13)
 plt.ylabel('Spannung U [mV]', fontsize=13)
+plt.title('Abb. [?]: Strom-Temperatur-Charakteristik', fontsize=16)
+plt.legend(frameon=True, fontsize = 12)
+#plt.savefig('figures//f16_abb_1.pdf',format='pdf')
+print('peak 1',popt1)
+print('peak 2',popt2)
+print('peak 3',popt3)
+print('peak 4',popt4)
+print('peak 5',popt5)
+print('peak 6',popt6)
+print('peak 7',popt7)
+print('peak 8',popt8)
+print('peak 9',popt9)
+#plt.show()
+plt.close()
+
+U=41.2
+U_err=0.1
+T=U/10*0.39
+T_err=U_err/10*0.39
+t=7.5
+t_err=0.01
+deltaT=T/t
+deltaT_err=np.sqrt((T_err/t)**2+(T/t**2*t_err)**2)
+print('deltaT=',deltaT,'+',deltaT_err)
+
+diff1=popt2[1]-popt1[1]
+diff1_err=np.sqrt(popt2[2]**2+popt1[2]**2)
+diff2=popt3[1]-popt2[1]
+diff2_err=np.sqrt(popt3[2]**2+popt2[2]**2)
+diff3=popt4[1]-popt3[1]
+diff3_err=np.sqrt(popt4[2]**2+popt3[2]**2)
+diff4=popt5[1]-popt4[1]
+diff4_err=np.sqrt(popt5[2]**2+popt4[2]**2)
+diff5=popt6[1]-popt5[1]
+diff5_err=np.sqrt(popt6[2]**2+popt5[2]**2)
+diff6=popt7[1]-popt6[1]
+diff6_err=np.sqrt(popt7[2]**2+popt6[2]**2)
+diff7=popt8[1]-popt7[1]
+diff7_err=np.sqrt(popt8[2]**2+popt7[2]**2)
+diff8=popt9[1]-popt8[1]
+diff8_err=np.sqrt(popt9[2]**2+popt8[2]**2)
+
+print("diff1=",diff1,'+',diff1_err)
+print("diff2=",diff2,'+',diff2_err)
+print("diff3=",diff3,'+',diff3_err)
+print("diff4=",diff4,'+',diff4_err)
+print("diff5=",diff5,'+',diff5_err)
+print("diff6=",diff6,'+',diff6_err)
+print("diff7=",diff7,'+',diff7_err)
+print("diff8=",diff8,'+',diff8_err)
+
+deltat=(diff1+diff2+diff3+diff4+diff5+diff6+diff7+diff8)/8
+deltat_err=np.sqrt(diff1_err**2+diff2_err**2+diff3_err**2+diff4_err**2+diff5_err**2+diff6_err**2+diff7_err**2+diff8_err**2)/8
+print("deltat=",deltat,"+",deltat_err)
+a=np.array([diff1,diff2,diff3,diff4,diff5,diff6,diff7,diff8])
+deltat_err=np.std(a)
+print("deltat=",deltat,"+",deltat_err,"statistischer Fehler wird hier genommen")
+
+deltavdeltaT=omegaFSR/(deltaT*deltat)
+deltavdeltaT_err=np.sqrt((omegaFSR_err/(deltaT*deltat))**2+(omegaFSR/(deltaT**2*deltat)*deltaT_err)**2+(omegaFSR/(deltaT*deltat**2)*deltat_err)**2)
+print('deltavdeltaT=',deltavdeltaT,'+',deltavdeltaT_err)
+
+##############################
+#Dopplerverbreiterte Laserspektroskopie##
+#####################################
+
+####### Temperaturscan #######
+
+x_2, trans_2 = np.loadtxt('data/absorption/tempscan_oab.csv', delimiter=',', usecols=(3, 4), unpack=True)
+x_3, trans_3 = np.loadtxt('data/absorption/tempscan_oab.csv', delimiter=',', usecols=(9, 10), unpack=True)
+x_4, trans_4 = np.loadtxt('data/absorption/tempscan_oab.csv', delimiter=',', usecols=(15, 16), unpack=True)
+x_5, trans_5 = np.loadtxt('data/absorption/tempscan_oab.csv', delimiter=',', usecols=(21, 22), unpack=True)
+trans_4=trans_4*15
+trans_3=trans_3*170
+
+popt1, pcov1 = curve_fit(Gauss, x_3[1226:1249], trans_3[1226:1249],p0=[40,2.47,0.004,4], absolute_sigma=True)
+popt2, pcov2 = curve_fit(Gauss, x_3[1286:1302], trans_3[1286:1302],p0=[40,2.59,0.004,4], absolute_sigma=True)
+popt3, pcov3 = curve_fit(Gauss, x_3[1344:1360], trans_3[1344:1360],p0=[40,2.70,0.004,4], absolute_sigma=True)
+popt4, pcov4 = curve_fit(Gauss, x_3[1400:1412], trans_3[1400:1412],p0=[40,2.81,0.004,4], absolute_sigma=True)
+popt5, pcov5 = curve_fit(Gauss, x_3[1456:1470], trans_3[1456:1470],p0=[40,2.92,0.004,4], absolute_sigma=True)
+popt6, pcov6 = curve_fit(Gauss, x_3[1507:1522], trans_3[1507:1522],p0=[40,3.02,0.004,4], absolute_sigma=True)
+popt7, pcov7 = curve_fit(Gauss, x_3[1561:1574], trans_3[1561:1574],p0=[40,3.13,0.004,4], absolute_sigma=True)
+popt8, pcov8 = curve_fit(Gauss, x_3[1613:1625], trans_3[1613:1625],p0=[40,3.24,0.004,4], absolute_sigma=True)
+popt9, pcov9 = curve_fit(Gauss, x_3[1665:1676], trans_3[1665:1676],p0=[40,3.34,0.004,4], absolute_sigma=True)
+popt10, pcov10 = curve_fit(Gauss, x_3[1716:1728], trans_3[1716:1728],p0=[40,3.44,0.004,4], absolute_sigma=True)
+popt11, pcov11 = curve_fit(Gauss, x_4[1211:1281], trans_4[1211:1281],p0=[-40,2.5,0.02,70], absolute_sigma=True)
+popt12, pcov12 = curve_fit(Gauss, x_4[1698:1754], trans_4[1698:1754],p0=[-40,3.43,0.02,70], absolute_sigma=True)
+
+#plt.plot(x_2[:],trans_2[:], linestyle='-',color='black', label='Messkurve')
+plt.plot(x_3[345:1891],trans_3[345:1891], linestyle='-',color='blue', label='Resonatormessung')
+plt.plot(x_4[345:1891],trans_4[345:1891], linestyle='-',color='red', label='Messkurve')
+plt.plot(x_5[345:1891],trans_5[345:1891], linestyle='-',color='green', label='Temperaturmodulation')
+plt.plot(x_3[1226:1249], Gauss(x_3[1226:1249],*popt1), color='turquoise', label='Gaussian')
+plt.plot(x_3[1286:1302], Gauss(x_3[1286:1302],*popt2), color='turquoise')
+plt.plot(x_3[1344:1360], Gauss(x_3[1344:1360],*popt3), color='turquoise')
+plt.plot(x_3[1400:1412], Gauss(x_3[1400:1412],*popt4), color='turquoise')
+plt.plot(x_3[1456:1470], Gauss(x_3[1456:1470],*popt5), color='turquoise')
+plt.plot(x_3[1507:1522], Gauss(x_3[1507:1522],*popt6), color='turquoise')
+plt.plot(x_3[1561:1574], Gauss(x_3[1561:1574],*popt7), color='turquoise')
+plt.plot(x_3[1613:1625], Gauss(x_3[1613:1625],*popt8), color='turquoise')
+plt.plot(x_3[1665:1676], Gauss(x_3[1665:1676],*popt9), color='turquoise')
+plt.plot(x_3[1716:1728], Gauss(x_3[1716:1728],*popt10), color='turquoise')
+plt.plot(x_4[1211:1281], Gauss(x_4[1211:1281],*popt11), color='black')
+plt.plot(x_4[1698:1754], Gauss(x_4[1698:1754],*popt12), color='black')
+
+plt.xlabel('Zeit t [s]', fontsize=13)
+plt.ylabel('Spannung U [V]', fontsize=13)
 plt.title('Abb. [?]: Strom-Frequenz-Charakteristik', fontsize=16)
 plt.legend(frameon=True, fontsize = 12)
 #plt.savefig('figures//f16_abb_1.pdf',format='pdf')
-plt.show()
+print('peak 1',popt1)
+print('peak 2',popt2)
+print('peak 3',popt3)
+print('peak 4',popt4)
+print('peak 5',popt5)
+print('peak 6',popt6)
+print('peak 7',popt7)
+print('peak 8',popt8)
+print('peak 9',popt9)
+print('peak 10',popt10)
+print('peak 11',popt11)
+print('peak 12',popt12)
+#plt.show()
 plt.close()
+
+diff1=popt2[1]-popt1[1]
+diff1_err=np.sqrt(popt2[2]**2+popt1[2]**2)
+diff2=popt3[1]-popt2[1]
+diff2_err=np.sqrt(popt3[2]**2+popt2[2]**2)
+diff3=popt4[1]-popt3[1]
+diff3_err=np.sqrt(popt4[2]**2+popt3[2]**2)
+diff4=popt5[1]-popt4[1]
+diff4_err=np.sqrt(popt5[2]**2+popt4[2]**2)
+diff5=popt6[1]-popt5[1]
+diff5_err=np.sqrt(popt6[2]**2+popt5[2]**2)
+diff6=popt7[1]-popt6[1]
+diff6_err=np.sqrt(popt7[2]**2+popt6[2]**2)
+diff7=popt8[1]-popt7[1]
+diff7_err=np.sqrt(popt8[2]**2+popt7[2]**2)
+diff8=popt9[1]-popt8[1]
+diff8_err=np.sqrt(popt9[2]**2+popt8[2]**2)
+diff9=popt10[1]-popt9[1]
+diff9_err=np.sqrt(popt10[2]**2+popt9[2]**2)
+
+print("diff1=",diff1,'+',diff1_err)
+print("diff2=",diff2,'+',diff2_err)
+print("diff3=",diff3,'+',diff3_err)
+print("diff4=",diff4,'+',diff4_err)
+print("diff5=",diff5,'+',diff5_err)
+print("diff6=",diff6,'+',diff6_err)
+print("diff7=",diff7,'+',diff7_err)
+print("diff8=",diff8,'+',diff8_err)
+print("diff9=",diff9,'+',diff9_err)
+
+deltat=(diff1+diff2+diff3+diff4+diff5+diff6+diff7+diff8+diff9)/9
+a=np.array([diff1,diff2,diff3,diff4,diff5,diff6,diff7,diff8,diff9])
+deltat_err=np.std(a)
+print("deltat=",deltat,"+",deltat_err,"statistischer Fehler wird hier genommen")
+
+diffsec=popt12[1]-popt11[1]
+diffsec_err=np.sqrt(popt12[2]**2+popt11[2]**2)
+print('diffsec=',diffsec,'+',diffsec_err)
+omegasec=diffsec/deltat*omegaFSR
+omegasec_err=np.sqrt((diffsec_err/deltat*omegaFSR)**2+(diffsec/deltat**2*omegaFSR*deltat_err)**2+(diffsec/deltat*omegaFSR_err)**2)
+print('omegasec=',omegasec,'+',omegasec_err)
+
