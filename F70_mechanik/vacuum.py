beaac60813fc641cd9bf96ccd1579330787e8655
drum4 <nils.schmitt@stud.uni-heidelberg.de>
2018-10-10 14:59:52 +0200

    update nmr

--- F70_mechanik/vacuum.py
+++ F70_mechanik/vacuum.py
@@ -36,14 +36,40 @@ p2 = np.array([7.5e-5,1e-4,2.5e-4])
 p2_err = np.array([0.1e-5,0.1e-4,0.1e-4])
 kap2 = vol2/t2*PA/p2
 kap2_err = vol2/t2**2*PA/p2*t2_err
+
 plt.errorbar(p1, kap1, xerr=p1_err, yerr=kap1_err,
              fmt='.', linewidth=1,
              linestyle='', color='black',
              label='Measuring data')
-#plt.errorbar(p2, kap2, xerr=p2_err, yerr=kap2_err,
- #            fmt='.', linewidth=1,
-  #           linestyle='', color='red',
-   #          label='Measuring data')
+plt.errorbar(p2, kap2, xerr=p2_err, yerr=kap2_err,
+             fmt='.', linewidth=1,
+             linestyle='', color='red',
+             label='Measuring data')
 plt.xscale('log')
-print(kap1,kap2)
 plt.show()
+
+
+
+
+
+##################
+#Leitwerte Rohr#
+##################
+p_ur = np.array([9.7e-6,1e-5,1.1e-5,1.5e-5,2.4e-5,3.3e-5,4.1e-5,9.2e-5,1.9e-4,3.4e-4,6.6e-4,2.9e-3,9.8e-3,2.5e-2,7.4e-2])
+p_ur_err = np.array([.1e-6,.1e-5,.1e-5,.1e-5,.1e-5,.1e-5,.1e-5,.1e-5,.1e-4,.1e-4,.1e-4,.1e-3,.1e-3,.1e-2,.1e-2])
+p_or = np.array([4.9e-4,7.5e-4,1.1e-3,2.5e-3,4.8e-3,7.6e-3,1e-2,2.5e-2,5e-2,7.6e-2,.1,.25,.5,.76,1.1])
+p_or_err = np.array([.1e-4,.1e-4,.1e-3,.1e-3,.1e-3,.1e-3,.1e-2,.1e-2,.1e-2,.1e-2,.1e-1,.1e-1,.1e-1,.1e-1,.1])
+
+
+
+
+
+
+
+
+
+
+##################
+#Leitwerte Blende#
+##################
+p_ob = np.array([8.5e-5,9.6e-5,2.4e-4,5.7e-4,7.8e-4,1.1e-3,2.4e-3,5.1e-3,7.6e-3,1e-2,2.6e-2,5.2e-2,7.5e-2,9.7e-2,.25,.5,.76])
