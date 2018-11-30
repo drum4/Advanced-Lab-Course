cfea3fa8ea6f54c90ed1154c3a1e941f469245ad
TimoKleinbek <kleinbek@stud.uni-heidelberg.de>
2018-11-30 17:24:46 +0100

    Anfang Rayleigh

--- F55_rayleigh/rayleigh.py
+++ F55_rayleigh/rayleigh.py
@@ -23,22 +23,11 @@ plt.rcParams['errorbar.capsize']=2
 #Teil 1#
 ##############
 
-x_2, trans_2 = np.loadtxt('data/transmissionpeak.csv', delimiter=',', usecols=(3, 4), unpack=True)
-
-plt.errorbar(current, mag_up, xerr=current_err, yerr=mag_up_err,
-             fmt='.', linewidth=1,
-             linestyle='', color='black',
-             label='Ascending measurement')
-plt.xlabel('Current I [A]', fontsize=13)
-plt.ylabel('Magnetic Field B [mT]', fontsize=13)
-plt.title('Fig. [1]: Hysteresis effect for magnets used', fontsize=16)
-plt.plot(current, prop(current,*popt_up), color='red', label='Linear fit ascending')
-plt.plot(current, prop(current,*popt_down), color='blue', label='Linear fit descending',
-         linestyle='--')
-plt.text(5,0.05,'Ascending/Descending Slope: \n %s \n %s'%(slope_a, slope_d),
-        bbox={'facecolor':'white', 'alpha':0.5, 'pad':10},
-        fontsize=13)
-plt.legend(frameon=True, fontsize = 12)
-plt.savefig('figures//f44_abb_1.pdf',format='pdf')
-#plt.show()
+Delta_2H, Delta_2H_StDev, Delta_18O, Delta_18O_StDev, Delta_17O, Delta_17O_StDev= np.loadtxt('data/teil1_40_50-Processed.txt', usecols=(2,3,4,5,6,7), skiprows=1, unpack=True)
+a=np.arange(1,86)
+plt.errorbar(a, Delta_2H,yerr=Delta_2H_StDev, fmt='.', linewidth=1, linestyle='', color='black')
+plt.xlabel('Nummer', fontsize=13)
+plt.ylabel('Delta_2H', fontsize=13)
+plt.title('Fig. [1]:Delta_2H', fontsize=16)
+plt.show()
 plt.close()
