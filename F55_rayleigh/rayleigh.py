de1b2b2a7de4bc063802791755ea4b2f24a30505
TimoKleinbek <kleinbek@stud.uni-heidelberg.de>
2018-11-30 20:08:18 +0100

    Datenauslesen

--- F55_rayleigh/rayleigh.py
+++ F55_rayleigh/rayleigh.py
@@ -22,6 +22,22 @@ plt.rcParams['errorbar.capsize']=2
 ##############
 #Teil 1#
 ##############
+VE=np.array([[],[],[],[],[],[]])
+Degre40_1252=np.array([[],[],[],[],[],[]])
+Degre40_1323=np.array([[],[],[],[],[],[]])
+Degre40_1352=np.array([[],[],[],[],[],[]])
+Degre40_1423=np.array([[],[],[],[],[],[]])
+Degre40_1453=np.array([[],[],[],[],[],[]])
+Degre40_1521=np.array([[],[],[],[],[],[]])
+Degre40_1552=np.array([[],[],[],[],[],[]])
+Degre50_1252=np.array([[],[],[],[],[],[]])
+Degre50_1324=np.array([[],[],[],[],[],[]])
+Degre50_1353=np.array([[],[],[],[],[],[]])
+Degre50_1424=np.array([[],[],[],[],[],[]])
+Degre50_1455=np.array([[],[],[],[],[],[]])
+Degre50_1522=np.array([[],[],[],[],[],[]])
+Degre50_1553=np.array([[],[],[],[],[],[]])
+
 
 Delta_2H, Delta_2H_StDev, Delta_18O, Delta_18O_StDev, Delta_17O, Delta_17O_StDev= np.loadtxt('data/teil1_40_50-Processed.txt', usecols=(2,3,4,5,6,7), skiprows=1, unpack=True)
 a=np.arange(1,86)
