cee470b6012d2ef8d23fd8da069cb1f4ddbefaf7
TimoKleinbek <kleinbek@stud.uni-heidelberg.de>
2018-12-02 22:04:42 +0100

    Präsentation Tobias Abele

--- F55_rayleigh/rayleigh.py
+++ F55_rayleigh/rayleigh.py
@@ -40,6 +40,27 @@ Degre50_1553=np.array([[],[],[],[],[],[]])
 
 
 Delta_2H, Delta_2H_StDev, Delta_18O, Delta_18O_StDev, Delta_17O, Delta_17O_StDev= np.loadtxt('data/teil1_40_50-Processed.txt', usecols=(2,3,4,5,6,7), skiprows=1, unpack=True)
+def auslesen(x,a,b):
+    x=np.array([[],[],[],[],[],[]])
+    for i in [a,b]:
+         x=np.insert(x,0,[[Delta_2H[a], Delta_2H_StDev[a], Delta_18O[a], Delta_18O_StDev[a], Delta_17O[a], Delta_17O_StDev[a]]],axis=1)
+    return x
+
+for i in [0,15,27,32,47,59,68]: #tatsaeliche Zeilennummer aus txt Datei -2
+    VE=np.insert(VE,0,[[Delta_2H[i], Delta_2H_StDev[i], Delta_18O[i], Delta_18O_StDev[i], Delta_17O[i], Delta_17O_StDev[i]]],axis=1)
+
+for i in [6,38]:
+    Degre40_1252=np.insert(Degre40_1252,0,[[Delta_2H[i], Delta_2H_StDev[i], Delta_18O[i], Delta_18O_StDev[i], Delta_17O[i], Delta_17O_StDev[i]]],axis=1)
+
+for i in [7,39]:
+    Degre40_1323=np.insert(Degre40_1323,0,[[Delta_2H[i], Delta_2H_StDev[i], Delta_18O[i], Delta_18O_StDev[i], Delta_17O[i], Delta_17O_StDev[i]]],axis=1)
+
+auslesen(Degre40_1352,10,11)
+print('Degre40_1352=',Degre40_1352)
+
+#print(VE)
+#print(Degre40_1252)
+#print(Degre40_1323)
 a=np.arange(1,86)
 plt.errorbar(a, Delta_2H,yerr=Delta_2H_StDev, fmt='.', linewidth=1, linestyle='', color='black')
 plt.xlabel('Nummer', fontsize=13)
