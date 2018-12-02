8dbd355dd6490624001896d9b9f65a382ade57db
TimoKleinbek <kleinbek@stud.uni-heidelberg.de>
2018-12-02 22:58:19 +0100

    Auslesefunktion geschrieben

--- F55_rayleigh/rayleigh.py
+++ F55_rayleigh/rayleigh.py
@@ -22,45 +22,40 @@ plt.rcParams['errorbar.capsize']=2
 ##############
 #Teil 1#
 ##############
-VE=np.array([[],[],[],[],[],[]])
-Degre40_1252=np.array([[],[],[],[],[],[]])
-Degre40_1323=np.array([[],[],[],[],[],[]])
-Degre40_1352=np.array([[],[],[],[],[],[]])
-Degre40_1423=np.array([[],[],[],[],[],[]])
-Degre40_1453=np.array([[],[],[],[],[],[]])
-Degre40_1521=np.array([[],[],[],[],[],[]])
-Degre40_1552=np.array([[],[],[],[],[],[]])
-Degre50_1252=np.array([[],[],[],[],[],[]])
-Degre50_1324=np.array([[],[],[],[],[],[]])
-Degre50_1353=np.array([[],[],[],[],[],[]])
-Degre50_1424=np.array([[],[],[],[],[],[]])
-Degre50_1455=np.array([[],[],[],[],[],[]])
-Degre50_1522=np.array([[],[],[],[],[],[]])
-Degre50_1553=np.array([[],[],[],[],[],[]])
-
-
 Delta_2H, Delta_2H_StDev, Delta_18O, Delta_18O_StDev, Delta_17O, Delta_17O_StDev= np.loadtxt('data/teil1_40_50-Processed.txt', usecols=(2,3,4,5,6,7), skiprows=1, unpack=True)
-def auslesen(x,a,b):
+
+def auslesen(a,b):
     x=np.array([[],[],[],[],[],[]])
     for i in [a,b]:
-         x=np.insert(x,0,[[Delta_2H[a], Delta_2H_StDev[a], Delta_18O[a], Delta_18O_StDev[a], Delta_17O[a], Delta_17O_StDev[a]]],axis=1)
+         x=np.insert(x,0,[[Delta_2H[i], Delta_2H_StDev[i], Delta_18O[i], Delta_18O_StDev[i], Delta_17O[i], Delta_17O_StDev[i]]],axis=1)
     return x
 
-for i in [0,15,27,32,47,59,68]: #tatsaeliche Zeilennummer aus txt Datei -2
-    VE=np.insert(VE,0,[[Delta_2H[i], Delta_2H_StDev[i], Delta_18O[i], Delta_18O_StDev[i], Delta_17O[i], Delta_17O_StDev[i]]],axis=1)
+Degre40_1252=auslesen(6,38)#tatsaeliche Zeilennummer aus txt Datei -2
+Degre40_1323=auslesen(8,40)
+Degre40_1352=auslesen(10,42)
+Degre40_1423=auslesen(18,50)
+Degre40_1453=auslesen(20,52)
+Degre40_1521=auslesen(28,60)
+Degre40_1552=auslesen(30,62)
+Degre50_1252=auslesen(7,39)
+Degre50_1324=auslesen(9,41)
+Degre50_1353=auslesen(17,49)
+Degre50_1424=auslesen(19,51)
+Degre50_1455=auslesen(21,53)
+Degre50_1522=auslesen(29,61)
+Degre50_1553=auslesen(31,63)
 
-for i in [6,38]:
-    Degre40_1252=np.insert(Degre40_1252,0,[[Delta_2H[i], Delta_2H_StDev[i], Delta_18O[i], Delta_18O_StDev[i], Delta_17O[i], Delta_17O_StDev[i]]],axis=1)
+def auslesenstandard(a,b,c,d,e,f,g):
+    x=np.array([[],[],[],[],[],[]])
+    for i in [a,b,c,d,e,f,g]:
+         x=np.insert(x,0,[[Delta_2H[i], Delta_2H_StDev[i], Delta_18O[i], Delta_18O_StDev[i], Delta_17O[i], Delta_17O_StDev[i]]],axis=1)
+    return x
 
-for i in [7,39]:
-    Degre40_1323=np.insert(Degre40_1323,0,[[Delta_2H[i], Delta_2H_StDev[i], Delta_18O[i], Delta_18O_StDev[i], Delta_17O[i], Delta_17O_StDev[i]]],axis=1)
+VE=auslesenstandard(0,15,27,32,47,59,68)
+print('VE=',VE)
+print(Degre40_1252)
 
-auslesen(Degre40_1352,10,11)
-print('Degre40_1352=',Degre40_1352)
 
-#print(VE)
-#print(Degre40_1252)
-#print(Degre40_1323)
 a=np.arange(1,86)
 plt.errorbar(a, Delta_2H,yerr=Delta_2H_StDev, fmt='.', linewidth=1, linestyle='', color='black')
 plt.xlabel('Nummer', fontsize=13)
